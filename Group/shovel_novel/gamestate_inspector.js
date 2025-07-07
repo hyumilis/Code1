3aa4986f80e795202ed8fdc158bf08e29a268862
hyumilis <102694635+hyumilis@users.noreply.github.com>
2025-07-07 11:22:25 +0200

    Add files via upload

---
+++ Group/shovel_novel/gamestate_inspector.js
@@ -0,0 +1,10 @@
+"use strict";
+var shovelnovel;
+(function (shovelnovel) {
+    console.log(parent);
+    console.log(parent.gamestate);
+    if (parent.gamestate == true) {
+        console.log("discovery");
+    }
+})(shovelnovel || (shovelnovel = {}));
+//# sourceMappingURL=gamestate_inspector.js.map
